f4e37a6f552109ae4c5581b85a47a37592bd6615
Kristian Freeman <kristian@kristianfreeman.com>
2014-01-28 17:20:31 -0800

    use https urls - I don't know how this is working but it is

--- lib/js_builder.rb
+++ lib/js_builder.rb
@@ -6,8 +6,8 @@ class JSBuilder
 
   def output
     @out = <<EOF
-document.write("<link rel='stylesheet' type='text/css' href='http://issues-embed.herokuapp.com/assets/from_gh.css'>");
-document.write("<link rel='stylesheet' type='text/css' href='http://issues-embed.herokuapp.com/assets/embed.css'>");
+document.write("<link rel='stylesheet' type='text/css' href='https://issues-embed.herokuapp.com/assets/from_gh.css'>");
+document.write("<link rel='stylesheet' type='text/css' href='https://issues-embed.herokuapp.com/assets/embed.css'>");
 document.write("<div class='timeline-comment'><div class='comment'>");
 document.write("<div class='timeline-comment-header'><div class='timeline-comment-header-text'><strong><a href='#{ issue.url }'>#{ issue.title }</a></strong> started by <a href='#{ issue.user_url }'>#{ issue.user.login }</a><div style='float: right;' class='gh-header-status #{ issue.state }'>#{ issue.state.capitalize }</div><br /><a href='#{ issue.repo_url }'>#{ issue.repo }</a> ##{ issue.issue_number }</div></div>");
 document.write("<div class='comment'><div class='comment-content'><div class='comment-body'>#{ issue.safe_body }</div></div></div>");
