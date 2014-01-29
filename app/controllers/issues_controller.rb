a876a11919922f50f0f93b69a6532ac464e15de5
Kristian Freeman <kristian@kristianfreeman.com>
2014-01-28 17:04:28 -0800

    Return js, not json, whoops

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -6,7 +6,7 @@ class IssuesController < ApplicationController
     @repo = params[:repo]
     @issue = params[:issue]
     @embed = IssueEmbed.new(@repo, @issue, @client)
-    render json: JSBuilder.new(@embed).output
+    render js: JSBuilder.new(@embed).output
 
   # rescue => ex
   #   render json: "Something was not valid. \nRepos should be in the format 'org/repo'.\nIssue numbers should be passed as strings or integers.\n"
